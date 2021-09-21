3bdef91c3a45b3c30eeb0469f8dfc053ae2e6db6
puneet123456789 <45749936+puneet123456789@users.noreply.github.com>
2021-09-21 09:06:22 -0500

    Add files via upload

---
+++ ApacheCollectionsTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.apachecollectionstest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApacheCollectionsTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
